4d0befedd0f399d50443f0ae007c287a6b9a1b86
emmanuel tindi <emmanueltindi66@gmail.com>
2024-03-18 15:36:01 +0300

    update file

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,7 +5,7 @@ import typing
 wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> typing.Coroutine[any]:
+def task_wait_random(max_delay: int) -> typing.Coroutine[any, any, any]:
     """returns a asyncio.Task"""
     task = asyncio.create_task(wait_r(max_delay))
     return task
